16b3e5c438856975f091977033d2225bb66af2a7
Samuel Frías <samuelfriher@gmail.com>
2025-03-14 09:21:33 +0000

    2024 - Day 2

---
+++ 2024/Day 2/part2.ts
@@ -0,0 +1,43 @@
+/**
+ * Advent of Code 2024 - Day 2
+ * https://adventofcode.com/2024/day/2
+ *
+ * @author Samuel Frías Hernández
+ */
+
+import { isSafe } from './tools';
+
+{
+  const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  let reports: number[][] = [];
+  readLine.on('line', (line: string) => {
+    if (line.length === 0) {
+      readLine.close();
+    } else {
+      reports.push(line.split(/\s+/).map(Number));
+    }
+  });
+
+  readLine.on('close', () => {
+    let count = 0;
+    for (const report of reports) {
+      let safe = false;
+      for (let i = 0; i < report.length; i++) {
+        if (isSafe(report.slice(0, i).concat(report.slice(i + 1)))) {
+          safe = true;
+          break;
+        }
+      }
+      if (safe) {
+        count++;
+      }
+    }
+    console.log(count);
+    process.exit(0);
+  });
+}
